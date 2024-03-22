b9e571a849ada2266d7869f090c73bb3780b382e
fwishyy <fwishytf2@gmail.com>
2024-03-22 10:47:36 +0800

    Update aim system

--- core/src/com/mygdx/actions/AttackAction.java
+++ core/src/com/mygdx/actions/AttackAction.java
@@ -12,6 +12,7 @@ public class AttackAction extends TemporalAction {
     public AttackAction() {
         setInterval(150);
     }
+
     @Override
     public boolean act() {
         if (actor instanceof Player) {
@@ -19,16 +20,18 @@ public class AttackAction extends TemporalAction {
             Vector2 origin = player.getVector2().cpy();
             origin.x += player.getWidth() / 2f;
             origin.y += player.getWidth() / 2f;
-            Fireball fireball = new Fireball("projectiles/fireball.png", origin.x, origin.y, "fireball", 1, 40, 0.05f);
 
             // TODO: maybe this still needs to be reworked more, but the aiming feels kinda jank
             // calculate the target direction using the Player's target vector
             // which represents where the cursor is aiming
             Vector2 target = player.getTarget();
 
-            if(target != null) {
-                Vector2 direction = target.cpy().sub(origin).nor();
-                fireball.setDirection(direction);
+            if (target != null) {
+                Vector2 direction = target.cpy().sub(origin);
+                Fireball fireball = new Fireball("projectiles/fireball.png", origin.x, origin.y, "fireball", 1, 40, 0.05f);
+                fireball.setX(fireball.getX() - fireball.getWidth() / 2f);
+                fireball.setY(fireball.getY() - fireball.getHeight() / 2f);
+                fireball.setDirection(direction.nor());
                 EntityAddedEvent.addEvent(new EntityAddedEvent(fireball));
             }
         }
